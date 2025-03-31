093e24bcf8455afa4b22880855d1f390244cfaae
Viktor <math.uncle.victor@gmail.com>
2025-03-31 10:58:48 +0300

    Version 1.2.1

--- qnbackend/server.js
+++ qnbackend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://questionnaire-theta-ten.vercel.app",
   credentials: true,
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: [ "Content-Type" ]
--- qnfrontend/src/config.js
+++ qnfrontend/src/config.js
@@ -1,4 +1,4 @@
-// const API_URL = "https://questionnaire-wv0v.onrender.com";
-const API_URL = "http://localhost:5000";
+const API_URL = "https://questionnaire-y0gb.onrender.com";
+// const API_URL = "http://localhost:5000";
 
 export default API_URL;
